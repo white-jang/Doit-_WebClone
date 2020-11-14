cf3e14701c5d29a1951254d53ad2131328e1dbe8
white-jang <galaxy__12@naver.com>
2020-11-14 14:41:38 +0900

    JS로 슬라이드 제작 (초기)

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-	
+	$("nav li").click(function () {
+        var id=$(this).attr("data-rol")
+        $("nav li").removeClass("on");
+        $(this).addClass("on")
+    })
 });
 

df2e0d60cd2e1dfaaa4cb4a91352288e6f49d9fb
white-jang <galaxy__12@naver.com>
2020-11-17 18:06:52 +0900

    메인페이지 애니메이션 추가

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
+    $("#container").addClass("start");
 	$("nav li").click(function () {
-        var id=$(this).attr("data-rol")
+        // nav 링크 클릭시 이벤트
+        $("#container").css("max-width", "100%"); 
+        // 메뉴 클릭할 때 #contianer의 width : 100% 만들기
+        var id=$(this).attr("data-rol");
         $("nav li").removeClass("on");
-        $(this).addClass("on")
-    })
+        $(this).addClass("on");
+        $(this).removeClass("prev this next"); 
+        // this 섹션 클릭시 기존의 prev, this, next 지우기
+        $("#"+id).addClass("this").prevAll().addClass("prev");
+        // nav에서 클릭한 메뉴와 매칭되는 id (menu1, menu2...)에 this 클래스 지정
+        // 그 앞의 모든 <section>에는 .prev 클래스 부여
+        $("#"+id).nextAll().addClass("next");
+        // nav에서 클릭한 메뉴와 매칭되는 id (menu1, menu2...) 뒤의
+        // 모든 <section>에는 .next 클래스 부여
+    });
+    $(".logo_box").click(function() {
+        // 로고 클릭시 이벤트, 메인 페이지로 돌아옴
+        $("nav li").removeClass("on");
+        $(".content").removeClass("prev this next"); 
+        $("#container").css("max-width", "1200px");
+    });
 });
 

e1b27f84a14d6a52eba52c71077d9747e99b9214
white-jang <galaxy__12@naver.com>
2021-01-22 17:01:56 +0900

    ~8chap

--- js/ui.js
+++ js/ui.js
@@ -43,5 +43,9 @@ $(document).ready(function(){
             }
         });
     });
+    $(".accordio_box ol li").click(function() {
+        $(".accordio_box ol li").removeClass("on");
+        $(this).addClass("on");
+    });
 });
 
